444af3555c60688cf8557b43714c16dc8d9592f1
ROGi0228 <84007090+ROGi0228@users.noreply.github.com>
2021-09-23 12:30:40 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// let btn = document.getElementById("btn");
+// btn.onclick = function(){
+//     let text = document.querySelector('p.intro')
+//     text.classList.add('red')
+// }
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.section__title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.condition').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('#tickets .condition').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+})
